91bfc82b30533e6fb449140a5439b1c404520c4a
Matt Dunlap <trinitrotoluene5@gmail.com>
2024-11-28 02:33:20 -0500

    Refactored the SumOfMultiples to allow for two arbitrary multiples.

--- src/ProjectEuler/SumOfMultiples.cs
+++ src/ProjectEuler/SumOfMultiples.cs
@@ -3,7 +3,7 @@ using System;
 namespace ProjectEuler;
 
 /// <summary>
-/// Static class for computing the sum of multiples of 3 and 5 less than an upper bound.
+/// Static class for computing the sum of two multiples less than an upper bound.
 /// </summary>
 /// <remarks>
 /// The original problem statement can be viewed at <see href="https://projecteuler.net/problem=1"/>.
@@ -11,15 +11,21 @@ namespace ProjectEuler;
 public static class SumOfMultiples
 {
     /// <summary>
-    /// Computes the sum of multiples of 3 and 5 less than <paramref name="upperBound"/>.
+    /// Computes the sum of two multiples less than <paramref name="upperBound"/>.
     /// </summary>
     /// <param name="upperBound">The upperBound of values to sum.</param>
-    /// <returns>A <see cref="ulong"/> representing the sum of all multiples of 3 and 5 less than <paramref name="upperBound"/>.</returns>
-    /// <exception cref="ArgumentOutOfRangeException">Thrown if <paramref name="upperBound"/> is zero.</exception>
-    public static ulong Compute(ulong upperBound)
+    /// <param name="multipleOne">The first multiple.</param>
+    /// <param name="multipleTwo">The second multiple.</param>
+    /// <returns>A <see cref="ulong"/> representing the sum of all multiples of <paramref name="multipleOne"/> and <paramref name="multipleTwo"/> less than <paramref name="upperBound"/>.</returns>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown if <paramref name="upperBound"/>, <paramref name="multipleOne"/>, or <paramref name="multipleTwo"/> is zero.</exception>
+    public static ulong Compute(ulong upperBound, ulong multipleOne, ulong multipleTwo)
     {
         ArgumentOutOfRangeException.ThrowIfZero(upperBound, nameof(upperBound));
-        return SumOfMultiplesLessThanUpperBound(3, upperBound) + SumOfMultiplesLessThanUpperBound(5, upperBound) - SumOfMultiplesLessThanUpperBound(15, upperBound);
+        ArgumentOutOfRangeException.ThrowIfZero(multipleOne, nameof(multipleOne));
+        ArgumentOutOfRangeException.ThrowIfZero(multipleTwo, nameof(multipleTwo));
+
+        var leastCommonMultiple = LeastCommonMultiple(multipleOne, multipleTwo);
+        return SumOfMultiplesLessThanUpperBound(multipleOne, upperBound) + SumOfMultiplesLessThanUpperBound(multipleTwo, upperBound) - SumOfMultiplesLessThanUpperBound(leastCommonMultiple, upperBound);
     }
 
     /// <summary>
@@ -36,5 +42,33 @@ public static class SumOfMultiples
 
         var iterations = upperBound / multiple + (upperBound % multiple == 0ul ? 0ul : 1ul);
         return multiple * iterations * (iterations - 1ul) / 2ul;
+    }
+
+    /// <summary>
+    /// Computes the Least Common Multiple (LCM) of two <see cref="ulong"/>s.
+    /// </summary>
+    /// <param name="a">The first value.</param>
+    /// <param name="b">The second value.</param>
+    /// <returns>A <see cref="ulong"/> representing the least common multiple of <paramref name="a"/> and <paramref name="b"/>.</returns>
+    private static ulong LeastCommonMultiple(ulong a, ulong b)
+    {
+        return a / GreatestCommonDivisor(a, b) * b;
+    }
+
+    /// <summary>
+    /// Computes the Greatest Common Divisor (GCD) of two <see cref="ulong"/>s.
+    /// </summary>
+    /// <param name="a">The first value.</param>
+    /// <param name="b">The second value.</param>
+    /// <returns>A <see cref="ulong"/> representing the greatest common divisor of <paramref name="a"/> and <paramref name="b"/>.</returns>
+    private static ulong GreatestCommonDivisor(ulong a, ulong b)
+    {
+        while (b != 0)
+        {
+            var temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
     }
 }
--- tests/ProjectEuler.UnitTests/SumOfMultiplesFixture.cs
+++ tests/ProjectEuler.UnitTests/SumOfMultiplesFixture.cs
@@ -11,29 +11,47 @@ internal sealed class SumOfMultiplesFixture
     {
         Assert.That(() =>
         {
-            _ = SumOfMultiples.Compute(0ul);
+            _ = SumOfMultiples.Compute(0ul, 1ul, 2ul);
+        }, Throws.TypeOf<ArgumentOutOfRangeException>());
+    }
+
+    [Test]
+    public void Should_throw_an_argument_out_of_range_exception_When_computing_with_a_first_multiple_of_zero()
+    {
+        Assert.That(() =>
+        {
+            _ = SumOfMultiples.Compute(1ul, 0ul, 2ul);
+        }, Throws.TypeOf<ArgumentOutOfRangeException>());
+    }
+
+    [Test]
+    public void Should_throw_an_argument_out_of_range_exception_When_computing_with_a_second_multiple_of_zero()
+    {
+        Assert.That(() =>
+        {
+            _ = SumOfMultiples.Compute(1ul, 2ul, 0ul);
         }, Throws.TypeOf<ArgumentOutOfRangeException>());
     }
 
     private static IEnumerable<TestCaseData> Source()
     {
-        yield return new TestCaseData(10ul)
+        yield return new TestCaseData(10ul, 3ul, 5ul)
             .SetName("Should return the sum of multiples of three and five less than 10.")
             .SetDescription("When computing the sum of multiples of three and five less than 10, then the sum of 23 should be returned.")
             .Returns(23ul);
-        yield return new TestCaseData(100ul)
+        yield return new TestCaseData(100ul, 3ul, 5ul)
             .SetName("Should return the sum of multiples of three and five less than 100.")
             .SetDescription("When computing the sum of multiples of three and five less than 100, then the sum of 2,318 should be returned.")
             .Returns(2318ul);
-        yield return new TestCaseData(1000ul)
+        yield return new TestCaseData(1000ul, 3ul, 5ul)
             .SetName("Should return the sum of multiples of three and five less than 1000.")
             .SetDescription("When computing the sum of multiples of three and five less than 1000, then the sum of 233,168 should be returned.")
             .Returns(233168ul);
     }
 
     [Test, TestCaseSource(nameof(Source))]
-    public ulong Should_return_the_sum_of_multiples_of_three_and_five_less_than_an_upper_bound(ulong upperBound)
+    public ulong Should_return_the_sum_of_two_multiples_less_than_an_upper_bound(ulong upperBound, ulong multipleOne, ulong multipleTwo)
     {
-        return SumOfMultiples.Compute(upperBound);
+        return SumOfMultiples.Compute(upperBound, multipleOne, multipleTwo);
     }
 }
