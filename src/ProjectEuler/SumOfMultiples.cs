f8de2787190abf8f359c72f1f68b291201372d72
Matt Dunlap <trinitrotoluene5@gmail.com>
2024-11-27 23:05:32 -0500

    Refactored to use a common function instead of using three separate functions for the sum of multiples of 3, 5, and 15.

--- src/ProjectEuler/SumOfMultiples.cs
+++ src/ProjectEuler/SumOfMultiples.cs
@@ -14,53 +14,19 @@ public static class SumOfMultiples
     /// <returns>A <see cref="ulong"/> representing the sum of all multiples of 3 and 5 less than 1000.</returns>
     public static ulong Compute()
     {
-        return SumOfMultiplesOfThreeLessThanOneThousand() + SumOfMultiplesOfFiveLessThanOneThousand() - SumOfMultiplesOfFifteenLessThanOneThousand();
+        return SumOfMultiplesLessThanOneThousand(3) + SumOfMultiplesLessThanOneThousand(5) - SumOfMultiplesLessThanOneThousand(15);
     }
 
     /// <summary>
-    /// Computes the sum of multiples of 3 less than 1000.
+    /// Computes the sum of multiples of <paramref name="multiple"/> less than 1000.
     /// </summary>
-    /// <returns>A <see cref="ulong"/> representing the sum of all multiples of 3 less than 1000.</returns>
-    private static ulong SumOfMultiplesOfThreeLessThanOneThousand()
+    /// <returns>A <see cref="ulong"/> representing the sum of all multiples of <paramref name="multiple"/> less than 1000.</returns>
+    private static ulong SumOfMultiplesLessThanOneThousand(ulong multiple)
     {
         ulong sum = 0ul;
         for (ulong i = 0; i < 1000; i++)
         {
-            if (i % 3 == 0)
-            {
-                sum += i;
-            }
-        }
-        return sum;
-    }
-
-    /// <summary>
-    /// Computes the sum of multiples of 5 less than 1000.
-    /// </summary>
-    /// <returns>A <see cref="ulong"/> representing the sum of all multiples of 5 less than 1000.</returns>
-    private static ulong SumOfMultiplesOfFiveLessThanOneThousand()
-    {
-        ulong sum = 0ul;
-        for (ulong i = 0; i < 1000; i++)
-        {
-            if (i % 5 == 0)
-            {
-                sum += i;
-            }
-        }
-        return sum;
-    }
-
-    /// <summary>
-    /// Computes the sum of multiples of 15 less than 1000.
-    /// </summary>
-    /// <returns>A <see cref="ulong"/> representing the sum of all multiples of 15 less than 1000.</returns>
-    private static ulong SumOfMultiplesOfFifteenLessThanOneThousand()
-    {
-        ulong sum = 0ul;
-        for (ulong i = 0; i < 1000; i++)
-        {
-            if (i % 15 == 0)
+            if (i % multiple == 0)
             {
                 sum += i;
             }
