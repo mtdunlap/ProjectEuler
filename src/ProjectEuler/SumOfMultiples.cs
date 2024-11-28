15d9541d6c20b7300e8150e9e29bc31f89f099e6
Matt Dunlap <trinitrotoluene5@gmail.com>
2024-11-27 23:21:27 -0500

    Parameterized the 'Compute()' method to take the upperBound as an argument. Updated test cases to also be parameterized.

--- src/ProjectEuler/SumOfMultiples.cs
+++ src/ProjectEuler/SumOfMultiples.cs
@@ -9,22 +9,25 @@ namespace ProjectEuler;
 public static class SumOfMultiples
 {
     /// <summary>
-    /// Computes the sum of multiples of 3 and 5 less than 1000.
+    /// Computes the sum of multiples of 3 and 5 less than <paramref name="upperBound"/>.
     /// </summary>
+    /// <param name="upperBound">The upperBound of values to sum.</param>
     /// <returns>A <see cref="ulong"/> representing the sum of all multiples of 3 and 5 less than 1000.</returns>
-    public static ulong Compute()
+    public static ulong Compute(ulong upperBound)
     {
-        return SumOfMultiplesLessThanOneThousand(3) + SumOfMultiplesLessThanOneThousand(5) - SumOfMultiplesLessThanOneThousand(15);
+        return SumOfMultiplesLessThanOneThousand(3, upperBound) + SumOfMultiplesLessThanOneThousand(5, upperBound) - SumOfMultiplesLessThanOneThousand(15, upperBound);
     }
 
     /// <summary>
-    /// Computes the sum of multiples of <paramref name="multiple"/> less than 1000.
+    /// Computes the sum of multiples of <paramref name="multiple"/> less than <paramref name="upperBound"/>.
     /// </summary>
-    /// <returns>A <see cref="ulong"/> representing the sum of all multiples of <paramref name="multiple"/> less than 1000.</returns>
-    private static ulong SumOfMultiplesLessThanOneThousand(ulong multiple)
+    /// <param name="multiple">The base multiple to use when computing the sum.</param>
+    /// <param name="upperBound">The upperBound of values to sum.</param>
+    /// <returns>A <see cref="ulong"/> representing the sum of all multiples of <paramref name="multiple"/> less than <paramref name="upperBound"/>.</returns>
+    private static ulong SumOfMultiplesLessThanOneThousand(ulong multiple, ulong upperBound)
     {
         ulong sum = 0ul;
-        for (ulong i = 0; i < 1000; i++)
+        for (ulong i = 0; i < upperBound; i++)
         {
             if (i % multiple == 0)
             {
--- tests/ProjectEuler.UnitTests/SumOfMultiplesFixture.cs
+++ tests/ProjectEuler.UnitTests/SumOfMultiplesFixture.cs
@@ -1,11 +1,29 @@
+using System.Collections.Generic;
+
 namespace ProjectEuler.UnitTests;
 
 [TestFixture]
 internal sealed class SumOfMultiplesFixture
 {
-    [Test(ExpectedResult = 233168ul)]
-    public ulong Should_return_the_sum_of_multiples_of_three_and_five_less_than_1000()
+    private static IEnumerable<TestCaseData> Source()
     {
-        return SumOfMultiples.Compute();
+        yield return new TestCaseData(10ul)
+            .SetName("Should return the sum of multiples of three and five less than 10.")
+            .SetDescription("When computing the sum of multiples of three and five less than 10, then the sum of 23 should be returned.")
+            .Returns(23ul);
+        yield return new TestCaseData(100ul)
+            .SetName("Should return the sum of multiples of three and five less than 100.")
+            .SetDescription("When computing the sum of multiples of three and five less than 100, then the sum of 2,318 should be returned.")
+            .Returns(2318ul);
+        yield return new TestCaseData(1000ul)
+            .SetName("Should return the sum of multiples of three and five less than 1000.")
+            .SetDescription("When computing the sum of multiples of three and five less than 1000, then the sum of 233,168 should be returned.")
+            .Returns(233168ul);
+    }
+
+    [Test, TestCaseSource(nameof(Source))]
+    public ulong Should_return_the_sum_of_multiples_of_three_and_five_less_than_an_upper_bound(ulong upperBound)
+    {
+        return SumOfMultiples.Compute(upperBound);
     }
 }
