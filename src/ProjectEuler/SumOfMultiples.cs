0cb2527174826179af06237eff2108b0592a5ce5
Matt Dunlap <trinitrotoluene5@gmail.com>
2024-11-28 02:05:07 -0500

    Further Update to documentation to reference the parameter 'upperBound' instead of the previous hard-coded value '1000'.

--- src/ProjectEuler/SumOfMultiples.cs
+++ src/ProjectEuler/SumOfMultiples.cs
@@ -3,7 +3,7 @@ using System;
 namespace ProjectEuler;
 
 /// <summary>
-/// Static class for computing the sum of multiples of 3 and 5 less than 1000.
+/// Static class for computing the sum of multiples of 3 and 5 less than an upper bound.
 /// </summary>
 /// <remarks>
 /// The original problem statement can be viewed at <see href="https://projecteuler.net/problem=1"/>.
